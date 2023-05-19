c0203cc3251d5f53b6c2d1099e19866f896cdcc0
denobot <33910674+denobot@users.noreply.github.com>
2023-05-19 23:01:01 +0900

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# REST API
+
+A template of REST API app using Oak framework
+
+
+Start the server with the command:
+
+```
+deno run --allow-net main.ts
+```
+
+This starts the server at http://localhost:8000/
+
+Try go to http://localhost:8000/api/Brachiosaurus or http://localhost:8000/api/
---
+++ main.ts
@@ -0,0 +1,31 @@
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+import data from "./data.json" assert { type: "json" };
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Welcome to dinosaur API!";
+  })
+  .get("/api", (context) => {
+    context.response.body = data;
+  })
+  .get("/api/:dinosaur", (context) => {
+    if (context?.params?.dinosaur) {
+      const found = data.find((item) =>
+        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
+      );
+      if (found) {
+        context.response.body = found;
+      } else {
+        context.response.body = "No dinosaurs found.";
+      }
+    }
+  });
+
+const app = new Application();
+app.use(oakCors()); // Enable CORS for All Routes
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
